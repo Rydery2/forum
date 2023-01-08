f3027f0da120e6d85f9ccab225db2a194e8955e7
Rydery2 <joeldutra@live.com.pt>
2023-01-08 02:39:07 -0100

    final

--- src/Domain/Answers/Answer.php
+++ src/Domain/Answers/Answer.php
@@ -5,24 +5,44 @@ namespace App\Domain\Answers;
 use App\Domain\Answers\Answer\AnswerId;
 use App\Domain\Questions\Question;
 use App\Domain\UserManagement\User;
+use App\Infrastructure\JsonApi\SchemaDiscovery\Attributes\AsResourceObject;
+use App\Infrastructure\JsonApi\SchemaDiscovery\Attributes\Attribute;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
 use JsonSerializable;
 
 
+#[
+    Entity,
+    Table(name: 'answers')
+]
+#[AsResourceObject(type: 'answers',links: [AsResourceObject::LINK_SELF],isCompound: true)]
 class Answer implements JsonSerializable
 {
 
-
+    #[Id, GeneratedValue(strategy: 'NONE') , Column(name: 'id', type: 'QuestionId')]
     private Question $question;
+    #[Id, GeneratedValue(strategy: 'NONE') , Column(name: 'id', type: 'AnswerId')]
     private AnswerId $answerId;
     /**
      * @var false
      */
+    #[Column( type: 'boolean', options:['default' => 0])]
+    #[Attribute (name:'isClosed')]
     private bool $isClosed = false;
     /**
      * @var false
      */
+    #[Column( type: 'boolean', options:['default' => 0])]
+    #[Attribute (name:'isArchived')]
     private bool $isArchived = false;
+    #[Id, GeneratedValue(strategy: 'NONE') , Column(name: 'id', type: 'UserId')]
     private User $owner;
+    #[Column(type: 'text')]
+    #[Attribute (name:'body')]
     private string $body;
 
     public function __construct(
